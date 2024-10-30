594335debb08b94f4da41a69a2526f94d3a3783c
11aciek <10337645+11aciek@users.noreply.github.com>
2024-10-30 01:07:25 +0100

    Add images

--- rps.js
+++ rps.js
@@ -5,12 +5,14 @@ little console game written for THE ODIN PROJECT assignment
 
 const ROUNDS = 5; // How many rounds. For now as constant, later player may choose no of rounds..
 const humanButton = Array.from(document.querySelectorAll(".human-button"));
-const playButton = document.querySelector("#computer");
+const computerField = document.querySelector("#computer");
 const statusText1 = document.querySelector("#text1");
 const statusText2 = document.querySelector("#text2");
 const statusText3 = document.querySelector("#text3");
 const infoRound = document.querySelector(".info-round");
 const result = document.querySelector(".result");
+
+const images = {rock: "./images/rock.png", paper: "./images/paper.png", scissors: "./images/scissors.png", start: "./images/q_mark.png"};
 
 let humanSelection = "";
 let computerSelection = "";
@@ -18,7 +20,6 @@ let humanScore = 0;
 let computerScore = 0;
 let round = 0;
 let newGame = true;
-
 
 function getComputerChoice() {
   return ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
@@ -28,47 +29,36 @@ function unselectButtons() {
   humanButton.forEach(btn => btn.classList.remove("active"));
 }
 
-humanButton.forEach(btn => btn.addEventListener("click", function(e) {
+humanButton.forEach(btn => btn.addEventListener("click", function() {
   if (newGame) {
 	playNewGame();
 	  }
-  humanSelection = e.target.id;
+  humanSelection = btn.id;
   unselectButtons();
   btn.classList.add("active");
-  }));
-
-playButton.addEventListener("click", function() {
-  if (newGame) {
-	playNewGame();
-	unselectButtons();
-  }
 
   computerSelection = getComputerChoice();
-  playButton.innerText = computerSelection;
-  if (humanSelection) {
-	playRound(humanSelection, computerSelection, ++round)
-	result.innerText = `${humanScore} : ${computerScore}`;
-  } else {
-	updateStatus("Please choose the option");
-	playButton.innerText = "press me";
-  }
+  computerField.src = images[computerSelection];
+  playRound(humanSelection, computerSelection, ++round)
+  result.innerText = `${humanScore} : ${computerScore}`;
+  
   if (humanScore > (computerScore + ROUNDS - round) ||
 	  computerScore > (humanScore + ROUNDS - round) ||
 	  round == ROUNDS) whoWin();
 
-});
+}));
   
 function playNewGame() {
-humanSelection = "";
-	computerSelection = "";
-	humanScore = 0;
-	computerScore = 0;
-	round = 0;
-	playButton.innerText = "press me";
-	infoRound.innerText = 0;
-	result.innerText = "0 : 0";
-	statusText1.style.color = "black";
-	newGame = false;
+  humanSelection = "";
+  computerSelection = "";
+  humanScore = 0;
+  computerScore = 0;
+  round = 0;
+  computerField.src = images["start"];
+  infoRound.innerText = 0;
+  result.innerText = "0 : 0";
+  statusText1.style.color = "black";
+  newGame = false;
 }
 
 function updateStatus(newText) {
@@ -90,20 +80,18 @@ function playRound(humanChoice, computerChoice, round) {
   } else if (result == "tie") {
 	updateStatus(`Tie! ${humanChoice} vs ${computerChoice}.`);
   }
-}
 
-function fight(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) return "tie";
+  function fight(humanChoice, computerChoice) {
+	if (humanChoice === computerChoice) return "tie";
 
-  switch (humanChoice) {
-	case "rock":
-	  return (computerChoice == "scissors") ? "win" : "loose";
-	case "paper":
-	  return (computerChoice == "rock") ? "win" : "loose";
-	case "scissors":
-	  return (computerChoice == "paper") ? "win" : "loose";
-	default:
-	  return "error"; // if player writes something different than rock, paper or scissors
+	switch (humanChoice) {
+	  case "rock":
+		return (computerChoice == "scissors") ? "win" : "loose";
+	  case "paper":
+		return (computerChoice == "rock") ? "win" : "loose";
+	  case "scissors":
+		return (computerChoice == "paper") ? "win" : "loose";
+	}
   }
 }
 
